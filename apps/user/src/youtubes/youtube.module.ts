c56dc230c2bca0cf45d1205be603fd97d38c1fd1
dqcuong <dqcuong19@gmail.com>
2025-07-21 11:25:20 +0700

    remove axios

--- apps/user/src/youtubes/youtube.module.ts
+++ apps/user/src/youtubes/youtube.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { YoutubesController } from './youtube.controller';
 import { YoutubesService } from './youtube.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [YoutubesController],
   providers: [YoutubesService],
 })
