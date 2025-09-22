cf3de54b1a14f956a18ba08dca0934e4d8d3b4c6
cuongdq-dev <dqcuong191@gmail.com>
2025-09-22 17:50:54 +0700

    Clean code

--- common/modules/src/media/index.ts
+++ common/modules/src/media/index.ts
@@ -3,5 +3,5 @@ export * from './media.controller';
 export * from './media.interface';
 export * from './media.module';
 export * from './media.service';
-export * from './multer_config.service';
+// export * from './multer_config.service';
 export * from './s3.service';
